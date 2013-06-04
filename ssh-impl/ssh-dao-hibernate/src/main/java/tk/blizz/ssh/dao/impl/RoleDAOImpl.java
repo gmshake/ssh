3089c9c53b0287c29dbbacf365b586e3cf79f650
Huang Zhenlei <zlei.huang@gmail.com>
2013-06-04 17:27:19 +0800

    add dao impl

---
+++ ssh-impl/ssh-dao-hibernate/src/main/java/tk/blizz/ssh/dao/impl/RoleDAOImpl.java
@@ -0,0 +1,17 @@
+package tk.blizz.ssh.dao.impl;
+
+import java.util.List;
+
+import tk.blizz.ssh.dao.RoleDAO;
+import tk.blizz.ssh.model.Role;
+
+public class RoleDAOImpl extends GenericHibernateDAO<Role, Long> implements
+		RoleDAO {
+
+	@Override
+	public List<Role> findByRoleName(String name) {
+		Role example = new Role().setName(name);
+		return findByExample(example);
+	}
+
+}
---
+++ ssh-impl/ssh-dao-hibernate/src/main/java/tk/blizz/ssh/dao/impl/UserDAOImpl.java
@@ -0,0 +1,17 @@
+package tk.blizz.ssh.dao.impl;
+
+import java.util.List;
+
+import tk.blizz.ssh.dao.UserDAO;
+import tk.blizz.ssh.model.User;
+
+public class UserDAOImpl extends GenericHibernateDAO<User, Long> implements
+		UserDAO {
+
+	@Override
+	public List<User> findByUserName(String name) {
+		User example = new User().setName(name);
+		return findByExample(example);
+	}
+
+}
