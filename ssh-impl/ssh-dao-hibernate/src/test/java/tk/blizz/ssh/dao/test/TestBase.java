e7f354847fc6f0a5a6307b5f98fd5372a30afaa2
Zhenlei Huang <zlei.huang@gmail.com>
2013-06-29 15:47:17 +0800

    add: add dao-hibernate base test

---
+++ ssh-impl/ssh-dao-hibernate/src/test/java/tk/blizz/ssh/dao/test/TestBase.java
@@ -0,0 +1,31 @@
+package tk.blizz.ssh.dao.test;
+
+import org.hsqldb.server.Server;
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Base Test for tests
+ * 
+ * @author zlei.huang@gmail.com (Huang Zhenlei)
+ * 
+ */
+public abstract class TestBase {
+	protected final Server server = new Server();
+
+	@Before
+	public void setup() {
+		this.server.setDatabaseName(0, "memdb");
+		this.server.setDatabasePath(0, "mem:memdb");
+		this.server.start();
+	}
+
+	@After
+	public void teardown() {
+		this.server.shutdown();
+	}
+
+	protected Server getServer() {
+		return this.server;
+	}
+}
